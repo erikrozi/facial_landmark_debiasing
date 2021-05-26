1f1316927e1cfb27bb35d66cfb0a2b41806763b3
Emily Jin <emilyjin@stanford.edu>
2021-05-26 09:23:31 -0700

    final loss functions

--- src/models/loss.py
+++ src/models/loss.py
@@ -1,88 +1,97 @@
-# Adversarial Domain Adaptation Loss
+"""
+Adversarial Domain Adaptation Loss
+
+Use the total_loss() function:
+N = number of samples
+L = number of landmarks
+K = number of sensitive attributes
+"""
+
 
 import torch
 import torch.nn as nn
 import math
 
 
-def total_loss_per_sample(output, target, feat_repr, target_attr, sens_attr, w=10, eps=2, alpha=1):
-    '''
+def total_loss(output, target, output_attr, target_attr, w=10, eps=2, alpha=1):
+    """
     Calculates the join-loss for adversarial domain adaptation
 
     PARAMETERS
-    output: output labels predicted by model
-    target: true labels
-    feat_repr: feature representations produced by the classifier
-    target_attr: true labels of all attributes
-    sens_attr: list of ints representing sensitive attributes
+    output: (N, 2L) tensor of landmark locations predicted by model (continuous)
+    target: (N, 2L) tensor of true landmark locations (continuous)
+    output_attr: (N, K, 2) tensor of predicted scores of sensitive attributes, given feature representations
+    target_attr: (N, K) tensor of true sensitive attribute labels. all entries are 0, 1
 
     HYPERPARAMETERS
     w: wing loss hyperparameter. sets the range of the non-linear part to be (-w, w). default 10
     eps: wing loss hyperparameter. controls curvature of the non-linear region. default 2
-    alpha: controls influence of confusion loss. defualt 1
-    '''
+    alpha: controls influence of confusion loss. default 1
+    """
     primary_loss = wing_loss(output, target, w, eps)
-    adversary_loss = adversarial_loss(feat_repr, target_attr, sens_attr)
-    confusion_loss = domain_confusion_loss(feat_repr, target_attr, sens_attr)
+    adversary_loss = adversarial_loss(output_attr, target_attr)
+    confusion_loss = domain_confusion_loss(output_attr)
 
     loss = primary_loss + adversary_loss + alpha * confusion_loss
     return loss
 
 
 def wing_loss(output, target, w=10, eps=2):
-    '''
-    Calculate average wing loss over all samples
+    """
+    Calculates average wing loss of all sample output/target differences
+    Calculates average wing loss of each landmark location
 
-    output: output labels predicted by model
-    target: true labels
+    PARAMETERS
+    output: (N, 2L) tensor of landmark locations predicted by model (continuous)
+    target: (N, 2L) tensor of true landmark locations (continuous)
     w: sets the range of the non-linear part to be (-w, w) (hyperparameter)
     eps: controls curvature of the non-linear region (hyperparameter)
 
     Default w, eps chosen to be the one presented in Feng et al., 2018 (https://arxiv.org/pdf/1711.06753.pdf)
-    '''
-    num_samples = output.shape[0]
+    """
+
     abs_diff = torch.abs(output - target)
     C = w - w * math.log(1 + w / eps)
 
     loss_per_sample_feature = torch.where(abs_diff < w, w * torch.ln(1 + abs_diff / eps), abs_diff - C)
-    loss = 1. / num_samples * torch.sum(loss_per_sample_feature)
+    loss_per_sample = loss_per_sample_feature.sum(dim=-1)
+    # loss_per_feature = loss_per_sample_feature.mean(dim=0)
+    loss = loss_per_sample.mean()
 
     return loss
 
 
-def adversarial_loss(feat_repr, target_attr, sens_attr):
-    '''
+def adversarial_loss(output_attr, target_attr):
+    """
     Calculates the adversarial loss of each sensitive attribute and returns the sum
 
-    feat_repr: feature representations produced by the classifier
-    target: true labels of all attributes
-    sens_attr: list of ints representing sensitive attributes
-    '''
-    softmax = nn.Softmax(dim=1) # check dim
-    probs = softmax(feat_repr)
+    PARAMETERS
+    output_attr: (N, K, 2) tensor of predicted scores of sensitive attributes, given feature representations
+    target_attr: (N, K) tensor of true sensitive attribute labels. all entries are 0, 1
+    """
 
-    # only get entries corresponding sensitive attributes, and compute loss
-    # need to check dimensions
-    sens_probs = probs[sens_attr]
-    sens_target = target_attr[sens_attr]
-    loss = sens_probs[:, sens_target].sum()
+    # compute softmax of each sensitive attribute for each sample
+    log_softmax = nn.LogSoftmax(dim=-1)
+    log_probs = log_softmax(output_attr)
+
+    # total loss
+    loss = log_probs[:, :, target_attr].sum()
     return loss
 
 
-def domain_confusion_loss(feat_repr, target_attr, sens_attr):
-    '''
+def domain_confusion_loss(output_attr):
+    """
     Calculates the domain confusion loss of each sensitive attribute and returns the sum
 
-    feat_repr: feature representations produced by the classifier
-    target: true labels of all attributes
-    sens_attr: list of ints representing sensitive attributes
-    '''
-    cross_entropy = nn.CrossEntropyLoss()
+    PARAMETERS
+    output_attr: (N, K, 2) tensor of predicted scores of sensitive attributes, given feature representations
+    target_attr: (N, K) tensor of true sensitive attribute labels. all entries are 0, 1
+    """
 
-    # only get entries corresponding sensitive attributes, and compute loss
-    sens_feat_repr = feat_repr[sens_attr]
-    sens_target = target_attr[sens_attr]
-    loss_per_attr = cross_entropy(sens_feat_repr, sens_target)
-    loss = loss_per_attr.sum()
+    # compute softmax of each sensitive attribute for each sample
+    log_softmax = nn.LogSoftmax(dim=-1)
+    log_probs = log_softmax(output_attr)
 
+    # total loss
+    loss = log_probs.mean()
     return loss
